5ed539ce10380b2caa44a7052c4d108f97f89fb8
Oleksandra Bardanova <tirz_hillelcsharp@outlook.com>
2025-10-28 09:37:30 +0100

    Add CalculatorLib and Calculator base class

---
+++ CalculatorApp/CalculatorLib/Calculator.cs
@@ -0,0 +1,35 @@
+﻿namespace CalculatorApp
+{
+    public static class Calculator
+    {
+       public static double LastResult { get; private set; }
+        public static double Add(double a, double b) 
+        {
+            LastResult = a + b; 
+            return LastResult;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            LastResult = a - b;
+            return LastResult;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            LastResult = a * b;
+            return LastResult;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+
+            LastResult = a / b;
+            return LastResult;
+        }
+    }
+}
